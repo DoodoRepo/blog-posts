1c102441d90bedee4603e14be8862916eb1a4da0
patryk <workingplace33@gmail.com>
2022-12-18 23:04:08 +0100

    created add,update,delete view

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 
 class Post(models.Model):
@@ -9,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])  # musi zostać dodane get_absolute_url przy używaniu formularza, poniewaz po wysłaniu post/get musi nastąpić przekierowanie
+                                                            # w tym wypadku reverse bierze bezwzględną ścieżkę do URL, jest to mapowanie na URL i odwołanie do odpowiedniego widoku
