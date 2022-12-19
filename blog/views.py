74d3ac1591979deff6b0ab8ee4f6694702f3ddd8
patryk <workingplace33@gmail.com>
2022-12-19 22:16:10 +0100

    user accounts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
